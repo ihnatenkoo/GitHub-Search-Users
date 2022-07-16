0923dc320008a73d5b2053f1ad66713108bde79a
Dmytro <ihnatenko.dev@gmail.com>
2022-07-16 20:49:08 +0300

    Concat defaultMiddleware with githubApi.middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,5 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { githubApi } from './github/github.api';
 
 export const store = configureStore({
-  reducer: { [githubApi.reducerPath]: githubApi.reducer }
+  reducer: { [githubApi.reducerPath]: githubApi.reducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware)
 });
