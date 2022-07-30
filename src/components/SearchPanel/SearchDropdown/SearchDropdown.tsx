82e383a6252cc415ed1725f3cb459dd7838c5b9e
Dmytro <ihnatenko.dev@gmail.com>
2022-07-30 17:52:24 +0300

    Create SearchDropdown component

---
+++ src/components/SearchPanel/SearchDropdown/SearchDropdown.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { IUsers } from '../../../types/types';
+
+interface ISearchDropdown {
+  users: Array<IUsers>;
+  getUserInfo: (username: string) => void;
+}
+
+const SearchDropdown: FC<ISearchDropdown> = ({ users, getUserInfo }) => {
+  return (
+    <ul className="list-none max-h-[200px]">
+      {users.map((user) => (
+        <li
+          className="py-3.5 sml:py-2  px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+          key={user.id}
+          onClick={() => getUserInfo(user.login)}
+        >
+          {user.login}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SearchDropdown;
